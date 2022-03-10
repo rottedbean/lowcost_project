131df3155a6a0a8ad24aabacee04e64e126ab4f8
yuyu0830 <yuyu0830@naver.com>
2022-03-10 16:06:26 +0900

    22/03/10
    존나 열심히 했다 ㄹㅇ
    검색 화면 대충 틀잡기
    메인화면 카드 띄우기
    장바구니 만들기
    등등

--- public/scripts/front.js
+++ public/scripts/front.js
@@ -1,1 +1,15 @@
-$(".nav_box").mouseover(function(){log("!");})
+var is_basket = false;
+function basket(){
+    if (is_basket){
+        $('#basket').css('top', '800px');
+        $('#curtain').css('background-color', 'rgba(255,255,255,0)');
+        $('#basket_button_img').attr("src","images/option_up.png")
+        is_basket = false;
+    }
+    else {
+        $('#basket').css('top', '350px');
+        $('#curtain').css('background-color', 'rgba(100,100,100,0.5)');
+        $('#basket_button_img').attr("src","images/option_down.png")
+        is_basket = true;
+    }
+};
